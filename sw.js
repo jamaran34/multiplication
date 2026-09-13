12625b501a32d68d9a5b6ca2efd31868860fa5a2
jamaran34 <jamaran34@yahoo.fr>
2026-09-13 14:37:18 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
    Après une modification du jeu, changez le numéro de version ci-dessous :
    les appareils récupéreront la nouvelle version à la prochaine ouverture. */
 
-const VERSION = "mc-v3";
+const VERSION = "mc-v7";
 const FICHIERS = [
   "./",
   "./index.html",
